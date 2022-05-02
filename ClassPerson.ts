aad9048c63910359e60adcbd829b0e87348e6d9c
KosukeYamamoto1024 <kosuke.yamamoto1024@gmail.com>
2022-05-02 14:14:31 +0900

    Calassお試し(ClassPerson)

---
+++ ClassPerson.ts
@@ -0,0 +1,13 @@
+class Person {
+    email: string;
+    name: string;
+    employeeId: string;
+
+    constructor(
+        email:string
+    ) {
+        this.email = email;
+        this.name = "テスト";
+        this.employeeId = "test";
+    }
+}
